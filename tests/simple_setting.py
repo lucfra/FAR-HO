f5be79d22ba1fd9f6f0e186495bbaca9bd8961df
luca <igor_mio@hotmail.it>
2018-05-17 16:55:49 +0200

    checked tf.nn.l2_loss

--- tests/simple_setting.py
+++ tests/simple_setting.py
@@ -15,12 +15,14 @@ v2 = tf.Variable([[-1., -2], [1., -21.]])
 lmbd = far.get_hyperparameter('lambda', 
                               initializer=tf.ones_initializer, shape=v2.get_shape())
 
-eta = far.get_hyperparameter('eta', 0.001)
+reg2 = far.get_hyperparameter('reg2', 0.1)
+
+eta = far.get_hyperparameter('eta', 0.1)
 beta1 = far.get_hyperparameter('beta1', 1.)
 beta2 = far.get_hyperparameter('beta2', 2.)
 
 # noinspection PyTypeChecker
-cost = tf.reduce_mean(v1**2) + tf.reduce_sum(lmbd*v2**2)
+cost = tf.reduce_mean(v1**2) + tf.reduce_sum(lmbd*v2**2) + reg2*tf.nn.l2_loss(v1)
 
 io_optim = far.AdamOptimizer(eta, tf.nn.sigmoid(beta1), tf.nn.sigmoid(beta2), epsilon=1.e-4)
 
@@ -41,6 +43,8 @@ print(far.utils.hyperparameters())
 tf.global_variables_initializer().run()
 print('hyperparameters:', ss.run(far.utils.hyperparameters()))
 
-farho.run(20)
+farho.run(100)
+
+print(ss.run(tf.trainable_variables()))
 
 print(ss.run(far.utils.hypergradients()))
